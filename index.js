f2f20224a2668c512bc0d7f05c19b82ca4a8b77a
Jonathan Shyaka <jonashyaka2@gmail.com>
2019-10-12 22:53:26 +0200

    Setup sequelize

---
+++ index.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-console */
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    status: 200,
+    message: 'NodeJS - Express - Postgres - Sequelize App Template',
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));

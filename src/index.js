c11f9cf1fcb77c79de97482a73a358ef9c3bc9d8
Jonathan Shyaka <jonashyaka2@gmail.com>
2019-10-13 10:21:36 +0200

    Add cors

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@
 import '@babel/polyfill';
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express();
+
+app.enable('trust proxy');
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());

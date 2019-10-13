320e6f0513ace836126c88fd7e667bc7b910d5bf
Jonathan Shyaka <jonashyaka2@gmail.com>
2019-10-13 10:18:08 +0200

    Add body-parser

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 /* eslint-disable no-console */
 import '@babel/polyfill';
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.status(200).json({

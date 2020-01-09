401c03fd0ad34030a579334bd85c6523bdca0d66
Jonathan Shyaka <jonashyaka2@gmail.com>
2020-01-09 13:25:07 +0200

    feature: viewing messages to a field

--- src/controllers/message.js
+++ src/controllers/message.js
@@ -1,5 +1,6 @@
 import response from '../helpers/response';
 import { add, get } from '../services/message';
+import { checkIfAdmin } from '../services/field';
 
 export default class MessageControllers {
   static async newMessage(req, res, next) {
@@ -14,7 +15,16 @@ export default class MessageControllers {
   static async getMessages(req, res, next) {
     try {
       const { username } = req.user;
-      const messages = await get(username);
+      const { field } = req.query;
+      let to;
+      if (field) {
+        const isAdmin = await checkIfAdmin(field, username);
+        if (!isAdmin) return response(res, 403, 'You don\'t have access to do that action', null, 'Forbidden');
+        to = field;
+      } else {
+        to = username;
+      }
+      const messages = await get(to);
       return response(res, 200, 'Retrieved successfully!', messages, null);
     } catch (error) {
       return next(error.message || error);
